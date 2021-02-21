fea9869de782615cf041cb5e93a83452761b4ee0
Taokanong <jarukit.chan@bumail.net>
2021-02-21 15:36:59 +0700

    Asset2

---
+++ Assets/Mainmenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    public void Beginer()
+    {
+        SceneManager.LoadScene("Beginner");
+    }
+    public void Intermediate()
+    {
+        SceneManager.LoadScene("Intermediate");
+    }
+    public void Expert()
+    {
+        SceneManager.LoadScene("Expert");
+    }
+}
